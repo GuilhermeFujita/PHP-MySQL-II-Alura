a6fbd08dfbf10757184762e3f5ed7f9ec8dbda88
GuilhermeFujita <fujitaguilherme@gmail.com>
2017-03-30 17:29:24 -0300

    Refatorando código

--- index.php
+++ index.php
@@ -1,34 +1,27 @@
-<?php include("cabecalho.php"); ?>			
-			<h1>Bem vindo!</h1>
-			<?php
-			if($isset($_GET["login"]) && $_GET["login"]==true){
-			?>
-				<p class="alert-success">Logado com sucesso</p>
-				<?php
-			}
-			?>
-			<?php
-			if($isset($_GET["login"]) && $_GET["login"] == false){
-			?>
-				<p class="alert-danger">Usuário ou senha inválida</p>
-				<?php
-			}
-			?>
-			<h2>Login</h2>
-			<form action="login.php" method="post">
-				<table class="table">
-					<tr>
-						<td>Email</td>
-						<td><input class="form-control" type="email" name="email"></td>
-					</tr>
-					<tr>
-						<td>Senha</td>
-						<td><input class="form-control" type="password" name="senha"></td>
-					</tr>
-					<tr>
-						<td><button type="submit" class="btn btn-primary">Login</button></td>
-					</tr>
-				</table>
-			</form>
+<?php include("cabecalho.php");
+      include("logica-usuario.php"); ?>
 
-<?php include("rodape.php"); ?>			
+<h1>Bem vindo!</h1>
+
+<?php if(usuarioEstaLogado()) { ?>
+    <p class="text-success">Você está logado como <?= usuarioLogado() ?>. <a href="logout.php">Deslogar</a></p>
+<?php } else { ?>
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <table class="table">
+            <tr>
+                <td>Email</td>
+                <td><input class="form-control" type="email" name="email"></td>
+            </tr>
+            <tr>
+                <td>Senha</td>
+                <td><input class="form-control" type="password" name="senha"></td>
+            </tr>
+            <tr>
+                <td><button class="btn btn-primary" type="submit">Login</button></td>
+            </tr>
+        </table>
+    </form>
+<?php } ?>
+
+<?php include("rodape.php"); ?>
